a937cdd0aaf9689bec62c9edf5978b667578701c
nduhiujames <nduhiujames8648@gmail.com>
2023-08-23 16:08:23 +0300

    Add first unit test

--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -2,11 +2,14 @@ package com.systechafrica;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) //default instance
+
+ public class AppTest {
 
     //? Reference=>app
     App app=new App();
@@ -18,7 +21,7 @@ public class AppTest {
         //then
         int expected=35;
         //verify that results =expected
-        Assertions.assertEquals(expected, result, "The sum of 25 + 10 should be 35");
+        Assertions.assertEquals(expected, result, "The sum of 25 + 10 should be 5");
 
 
 
