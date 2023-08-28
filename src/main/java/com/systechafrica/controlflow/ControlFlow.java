2b463bf883dc7fd2a82579b16b802bb0735bc2d8
nduhiujames <nduhiujames8648@gmail.com>
2023-08-28 16:47:08 +0300

    Control flow statements

---
+++ src/main/java/com/systechafrica/controlflow/ControlFlow.java
@@ -0,0 +1,63 @@
+package com.systechafrica.controlflow;
+
+import java.util.logging.Logger;
+
+import com.systechafrica.operators.OperatorsDemo;
+
+public class ControlFlow {
+    private static final Logger LOGGER= Logger.getLogger(OperatorsDemo.class.getName());
+
+    public void ifelseStatement(){
+    
+
+          int maths=70;
+        char grade;
+
+        if(maths>= 70){
+            grade='A';
+        }
+        //add conditions here to assign grade variable as expected
+        LOGGER.info("Student Grade ");
+    }
+    public void switchStatement(){
+        String day="FRIDAY";
+        switch(day){
+            case"MONDAY":
+            LOGGER.info("Monday Working Day");
+            break;
+
+            case"TUESDAY":
+            LOGGER.info("Tuesday Working Day");
+            break;
+
+            case"WEDNESDAY":
+            LOGGER.info("Wednesdy Working Day");
+            break;
+
+            case"THURSDAY":
+            LOGGER.info("Thursday Working Day");
+            break;
+
+            case"FRIDAY":
+            LOGGER.info("Friday Working Day");
+            break;
+
+             case"SATURDAY":
+             case"SUNDAY":
+            LOGGER.info("Sherehe ");
+            break;
+
+        }
+
+    }
+    public static void main(String[] args) {
+        ControlFlow app= new ControlFlow();
+        app.ifelseStatement();
+        app.switchStatement();
+      
+
+
+        
+    }
+    
+}
