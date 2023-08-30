04433bab208662b73abdce3ed4152a92d4bf59e7
nduhiujames <nduhiujames8648@gmail.com>
2023-08-30 12:28:07 +0300

    single dimentional arrays

---
+++ src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
@@ -0,0 +1,28 @@
+package com.systechafrica.arrays;
+
+import java.util.logging.Logger;
+
+import com.systechafrica.stringandstringbuilder.StringClassDemo;
+
+public class WorkingWithArrays {
+    private static final Logger LOGGER= Logger.getLogger(WorkingWithArrays.class.getName());
+    public void singleDimensionalArray(){
+        int[] numbers={0,1,2,3,4,5};
+        String[] nickNames={"Jimi","Jaymoh","james","Maina"};
+        //for (int i=0;i<numbers.length;i++){
+            //LOGGER.info(""+numbers[i]);
+
+        //}
+        LOGGER.info(nickNames[0]);
+        LOGGER.info(nickNames[1]);
+        LOGGER.info(nickNames[2]);
+        
+
+
+    }
+    
+public static void main(String[] args) {
+    
+}
+    
+}
