3a93d500f499d13b1c4cc20ffc461f4ead0af99c
nduhiujames <nduhiujames8648@gmail.com>
2023-09-09 20:00:26 +0300

    classes and packages

---
+++ src/main/java/com/systechafrica/part2/accessmodifiers/AccessModifiersDemo.java
@@ -0,0 +1,14 @@
+package com.systechafrica.part2.accessmodifiers;
+
+import java.util.logging.Logger;
+
+
+public class AccessModifiersDemo {
+    private static final Logger LOGGER= Logger.getLogger(AccessModifiersDemo.class.getName());
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
---
+++ src/main/java/com/systechafrica/part2/accessmodifiers/Person.java
@@ -0,0 +1,12 @@
+package com.systechafrica.part2.accessmodifiers;
+
+public class Person {
+    private String name;
+    private String email;
+    
+    public void printPersonalDetails(){
+        System.out.println(email +""+ name);
+
+    }
+
+}
