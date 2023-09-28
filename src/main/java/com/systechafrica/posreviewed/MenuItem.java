2edbd92fade7b14effd70c71215940bcdc29f3cc
nduhiujames <nduhiujames8648@gmail.com>
2023-09-28 19:14:05 +0300

    menu item class

---
+++ src/main/java/com/systechafrica/posreviewed/MenuItem.java
@@ -0,0 +1,29 @@
+package com.systechafrica.posreviewed;
+
+public class MenuItem {
+    private String itemCode;
+    private int quantity;
+    private double unitPrice;
+
+    public MenuItem(String itemCode, int quantity, double unitPrice) {
+        this.itemCode = itemCode;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotalValue() {
+        return quantity * unitPrice;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+}
