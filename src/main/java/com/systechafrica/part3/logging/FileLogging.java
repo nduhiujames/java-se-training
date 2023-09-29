ac58e21c964cc7b973a3a05ada28e97b25982ff3
nduhiujames <nduhiujames8648@gmail.com>
2023-09-29 14:55:43 +0300

    logginginto a file with custom format

---
+++ src/main/java/com/systechafrica/part3/logging/FileLogging.java
@@ -0,0 +1,38 @@
+package com.systechafrica.part3.logging;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+import com.systechafrica.controlflow.ControlFlow;
+
+public class FileLogging {
+    private static final Logger LOGGER= Logger.getLogger(FileLogging.class.getName());
+    public static void main(String[] args) throws SecurityException, IOException {
+        FileHandler fileHandler=new FileHandler("log.txt");
+        CustomFormatter formatter=new CustomFormatter();
+        LOGGER.addHandler(fileHandler);
+        fileHandler.setFormatter(formatter);
+        LOGGER.info("Info message logging\n");
+        LOGGER.severe("error message logging\n");
+        LOGGER.warning("warning message logging\n");
+    }
+    public static void test(){
+        LOGGER.info("Info message logging\n");
+        LOGGER.severe("error message logging\n");
+        LOGGER.warning("warning message logging\n");
+
+    }
+    static class CustomFormatter extends Formatter{
+
+        @Override
+        public String format(LogRecord record) {
+            String method=record.getSourceMethodName();
+            String level=record.getLevel().getName();
+            String message=record.getMessage();
+            System.out.println(record.getMessage());
+            return method+ "|"+level+ "|"+message;
+        }
+
+    }
+    
+}
